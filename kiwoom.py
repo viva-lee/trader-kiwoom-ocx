a1fb4f83b5a7d5bf84a5fc4b1fb4c91aa141d558
viva-lee <phoenix71405@gmail.com>
2022-07-15 22:12:35 +0900

    first commit

---
+++ kiwoom.py
@@ -0,0 +1,15 @@
+from PyQt5.QAxContainer import *
+from PyQt5.QtCore import *
+
+class Kiwoom(QAxWidget):
+    def __init__(self):
+        super().__init__()
+        print("Kiwoom() class start.")
+        
+        # 초기 세팅 함수들 바로 실행
+        self.get_ocx_instance()
+        self.event_slots()
+        self.signal_login_commConnect()
+        
+        def event_slots(self):
+            self.OnEventConnect.connect(self.login_slot)
