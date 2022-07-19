33616b18d9df41489252b9a57b732539bcc0c54d
viva-lee <phoenix71405@gmail.com>
2022-07-19 22:48:23 +0900

    예수금 확인 요청 시그널

--- kiwoom/kiwoom.py
+++ kiwoom/kiwoom.py
@@ -13,11 +13,26 @@ class Kiwoom(QAxWidget):
         ################################
         
         ################################
+        # 계좌 관련된 변수
+        self.account_num = None # 계좌번호 담아줄 변수
+        self.deposit = 0 # 예수금
+        self.use_money = 0 # 실제 투자금
+        self.use_money_percent = 0.5 # 예수금에서 사용할 비율
+        self.output_deposit = 0 # 출력가능 금액
+        ################################
+        
+        ################################
+        # 요청 스크린 번호
+        self.screen_my_info = "2000" # 계좌 관련 스크린 번호
+        ################################
+        
+        ################################
         # 초기 세팅 함수들 바로 실행
         self.get_ocx_instance() # ocx 방식을 파이썬이 사용할 수 있게 변환해 주는 함수
         self.event_slots() # 키움과 연결하기 위한 시그널/슬롯 모음
         self.signal_login_commConnect() # 로그인 요청 함수
         self.get_account_info() # 계좌번호 가져오기
+        self.detail_account_info() # 예수금 요청 시그널 포함
         ################################
         
     def get_ocx_instance(self):
@@ -42,3 +57,10 @@ class Kiwoom(QAxWidget):
         
         self.account_num = account_num
         print("계좌번호 : %s" % account_num)
+        
+    def detail_account_info(self, sPrevNext="0"):
+        self.dynamicCall("SetInputValue(QString, QString)", "계좌번호", self.account_num)
+        self.dynamicCall("SetInputValue(QString, QString)", "비밀번호", "0000")
+        self.dynamicCall("SetInputValue(QString, QString)", "비밀번호입력매체구분", "00")
+        self.dynamicCall("SetInputValue(QString, QString)", "조회구분", "1")
+        self.dynamicCall("CommRqData(QString, Qstring, int, QString)", "예수금상세현황요청", "opw00001", sPrevNext, self.screen_my_info)
